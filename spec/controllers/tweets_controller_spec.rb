a46a8584b57f0c84a47711fd9e76b531f0be62c9
Raphael Onofre <raphael.onofre@gmail.com>
2017-10-04 15:36:12 -0300

    feat: create controller test for new action and model test for validate the description's length

--- spec/controllers/tweets_controller_spec.rb
+++ spec/controllers/tweets_controller_spec.rb
@@ -8,10 +8,7 @@ RSpec.describe TweetsController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    it "get assigns a new Tweet do @tweet" do
-      get :new
-      Tweet.should_receive(:new).and_return(Tweet.new)
-    end
+
   end
 
   describe "GET #create" do
@@ -22,26 +19,3 @@ RSpec.describe TweetsController, type: :controller do
   end
 
 end
-
-
-#
-# require 'rails_helper'
-# require 'spec_helper'
-#
-#
-# RSpec.describe TweetsController, :type => :controller do
-#   describe "GET #new" do
-#     it "assigns a new Tweet to @tweet" do
-#       get :new
-#       Tweet.should_receive(:new).and_return(Article.new)
-#     end
-#     # it "renders the :new template" do
-#     #    get :new
-#     #    response.should render_template :new
-#     # end
-#   end
-#   # describe "creates a tweet" do
-#   #     it "saves the new contact in the database"
-#   #     it "redirects to the home page"
-#   # end
-# end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,27 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'shoulda/matchers'
+::Bundler.require(:default, :test)
+
+require "shoulda/matchers"
+::Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    # Choose a test framework:
+    with.test_framework :rspec
+    #with.test_framework :minitest
+    #with.test_framework :minitest_4
+    #with.test_framework :test_unit
+
+    # Choose one or more libraries:
+    with.library :active_record
+    with.library :active_model
+    #with.library :action_controller
+    # Or, choose the following (which implies all of the above):
+    #with.library :rails
+  end
+end
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
