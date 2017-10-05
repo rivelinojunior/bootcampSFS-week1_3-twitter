4316ba8930d5ecdb9d7575e48edfe3e4686230f7
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-10-04 21:08:38 -0400

    feat: create scope timeline

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,5 +1,15 @@
 class Tweet < ApplicationRecord
+
+  scope :timeline, -> user_id { get_timeline(user_id) }
+
   belongs_to :user
 
-  validates :description, presence: true, length: { maximum: 280 }
+  validates :description, presence: true, length: {maximum: 280}
+
+  def self.get_timeline(user_id)
+    user = User.find_by(id: user_id)
+    user_ids = user.following.ids
+    user_ids << user.id
+    Tweet.where(user_id: user_ids).order(created_at: :desc)
+  end
 end
