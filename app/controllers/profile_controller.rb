4df2a1154543fd098c71088143298670f47cf250
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-10-05 00:16:49 -0400

    feat: create action show in profile controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -1,2 +1,12 @@
 class ProfileController < ApplicationController
+  before_action :set_user, only: [:show]
+
+  def show
+  end
+
+  private
+
+  def set_user
+    @user = User.find_by(id: params[:id])
+  end
 end
