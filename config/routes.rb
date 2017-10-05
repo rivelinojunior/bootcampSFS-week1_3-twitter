27ef950cd24acb9e96c69f5c504d7504181ead5b
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-10-05 19:50:44 -0400

    wip: alter route for production env

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,6 @@ Rails.application.routes.draw do
   root 'tweets#index'
 
   devise_for :users
-  resources :users do
-    member do
-      get :following, :followers
-    end
-  end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :tweets, only: [:index, :new, :create]
   resources :profile, only: [:show]
