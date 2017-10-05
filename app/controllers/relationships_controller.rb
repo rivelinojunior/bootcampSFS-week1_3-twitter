c3323fc77267242cf81bb1229e8a41179b1e8c8c
Breno <brenosales@gmail.com>
2017-10-04 21:11:24 -0300

    feat: Create model Relationships (follow/unfollow)

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,14 @@
+class RelationshipsController < ApplicationController
+
+    def create
+        user = User.find(params[:followed_id])
+        current_user.follow(user)
+        redirect_to(:back)
+    end
+
+    def destroy
+        user = Relationships.find(params[:id]).followed
+        current_user.unfollow(user)
+        redirect_to(:back)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
   end
 
   devise_for :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
