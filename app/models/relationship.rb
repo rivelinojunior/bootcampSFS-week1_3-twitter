b0706780443cd17720b19f97b509216ae5e2fab5
lucasduarte <lucasduarte599@gmail.com>
2017-10-07 01:40:16 -0300

    feat: prevents the user from following the same user

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,6 @@ class Relationship < ActiveRecord::Base
     belongs_to :followed, class_name: "User"
     validates :follower_id, presence: true
     validates :followed_id, presence: true
+
+    validates :follower_id, uniqueness: { scope: :followed_id }
 end
