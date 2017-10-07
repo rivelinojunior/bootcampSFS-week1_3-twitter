2ff505d6bc73a842fb0868e42ecd488ec7101c5c
Rivelino Junior <rv.fernandes.junior@gmail.com>
2017-10-06 21:44:48 -0400

    feat: create alias for route like and dislike

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :tweets, only: [:index, :new, :create]
   resources :profile, only: [:show]
 
-  put 'tweets/like/:id' => 'tweets#like'
-  delete 'tweets/dislike/:id' => 'tweets#dislike'
+  put 'tweets/like/:id' => 'tweets#like', as: 'like_path'
+  delete 'tweets/dislike/:id' => 'tweets#dislike', as: 'dislike_path'
   get 'tweets/search' => 'tweets#search'
 end
